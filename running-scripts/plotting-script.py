7cba472bf3b497eba33090272d015bcae67c8696
Owen Pembery <o.r.pembery@bath.ac.uk>
2018-12-18 13:28:07 +0000

    This version works for constant background.

--- running-scripts/plotting-script.py
+++ running-scripts/plotting-script.py
@@ -8,7 +8,7 @@ this_directory = './'
 csv_list = []
 for filename in listdir():
     print(filename)
-    if fnmatch(filename,'*CSV'):
+    if fnmatch(filename,'*csv'):
         csv_list.append(this_directory + filename)
 
 info_data = utils.read_repeats_from_csv(this_directory+csv_list[0])
@@ -24,76 +24,58 @@ names_list.remove('num_repeats')
         
 all_csvs_df = utils.csv_list_to_dataframe(csv_list,names_list)
 
-
-max_len = 0
-for ii in all_csvs_df.index:
-    this_output = np.unique(all_csvs_df.loc[ii,:].values)
-    max_len = max(max_len,len(this_output))
-    all_csvs_df.loc[ii,:(len(this_output)-1)] = this_output
-    all_csvs_df.loc[ii,len(this_output):] = np.nan
+# This only comes into play when doing a lot of runs
+#max_len = 0
+#for ii in all_csvs_df.index:
+#    this_output = np.unique(all_csvs_df.loc[ii,:].values)
+#    max_len = max(max_len,len(this_output))
+#    all_csvs_df.loc[ii,:(len(this_output)-1)] = this_output
+#    all_csvs_df.loc[ii,len(this_output):] = np.nan
 
 # Cut the nans
-#for ii in range(max_len,10000):
 
-all_csvs_dropped_df = all_csvs_df.drop(labels=[ii for ii in range(max_len,10000)],axis=1)
+#all_csvs_dropped_df = all_csvs_df.drop(labels=[ii for ii in range(max_len,10000)],axis=1)
 
-#def name2levels(name):
-#    return all_csvs_dropped_df.index.levels[all_csvs_df.index.names.index(name)]
 
-#for n_pre_type in name2levels('n_pre_type'):
-#    for noise_master in name2levels('noise_master'):
-        # Start plot
-#        for modifier in name2levels('modifier'):
-#            for k in name2levels('k'):
-#                data = all_csvs_df.xs((n_pre_type,noise_master,modifier,k),level=('n_pre_type','noise_master','modifier','k'),drop_level=False)
-                # idea from http://stackoverflow.com/questions/20084487/ddg#20084590
-#                data.reset_index().plot()
 
-#data = all_csvs_dropped_df.xs((n_pre_type,noise_master,modifier,k),level=('n_pre_type','noise_master','modifier','k'),drop_level=False)
-
-# from https://stackoverflow.com/questions/28203643/plotting-multiple-lines-in-ipython-pandas-produces-multiple-plots
-
-#ax = plt.axes()
-
-n_pre_type = 'constant'
-
-#noise_master = '(0.0, 0.1)'
-noise_master = '(0.1, 0.0)'
-
-#p = 1
-def plt_gmres(n_pre_type,noise_master):
-    ks = [10.0,20.0,30.0]
+def plt_gmres(n_pre_type,noise_master,ks,modifiers):
     cols = ['k','r','b','g']
     markers = ['o','v','^']
-    #modifiers = ['(0.0, 0.0, 1.0, -1.0)','(0.0, 0.0, 1.0, -0.5)','(0.0, 0.0, 1.0, 0.0)']
-    modifiers = ['(1.0, -1.0, 0.0, 0.0)','(1.0, -0.5, 0.0, 0.0)','(1.0, 0.0, 0.0, 0.0)']
     handles = []
     for ii in range(len(modifiers)):
         modifier = modifiers[ii]
         for k in ks:
-            data = all_csvs_dropped_df.xs((n_pre_type,noise_master,modifier,k),level=('n_pre_type','noise_master','modifier','k'),drop_level=False)
+            data = all_csvs_df.xs((n_pre_type,noise_master,modifier,k),level=('n_pre_type','noise_master','modifier','k'),drop_level=False)
             for jj in data.columns:
-                if not np.isnan(data.iloc[0,jj]):
-                    #print(data.iloc[0,jj],flush=True)
-                    #print(data.reset_index().loc[0,'k'])
-                    #data.reset_index().plot.scatter(x='k',y=jj,c=cols[ii],ax=ax)
-                    if k == ks[0] and jj ==0:
-                        handles.append(plt.scatter(x=data.reset_index().loc[0,'k'],y=data.iloc[0,jj],c=cols[ii],marker=markers[ii],label='Modifier = '+modifier))
-                    else:
-                        plt.scatter(x=data.reset_index().loc[0,'k'],y=data.iloc[0,jj],c=cols[ii],marker=markers[ii],label='Modifier = '+modifier)    
-                    #print(data.iloc[0,jj] == np.nan)
-    #                print(data.iloc[0,jj])
-    #        data.reset_index().plot.scatter(x='k',y=data.columns,c=cols[ii],ax=ax)
-    plt.legend(handles=handles,loc=5)
-    # legend placement - ok for now
-    # title
-    title_string = 'n_pre_type = ' + n_pre_type + ' and noise_master = ' + noise_master
+                if k == ks[0] and jj == 0:
+                    handles.append(plt.scatter(x=data.reset_index().loc[0,'k'],y=data.iloc[0,jj],c=cols[ii],marker=markers[ii],label='Modifier = '+modifier))
+                else:
+                    plt.scatter(x=data.reset_index().loc[0,'k'],y=data.iloc[0,jj],c=cols[ii],marker=markers[ii],label='Modifier = '+modifier)
+    #labels = # noise = noise_master non-zero $k^whatever$ # for now I'll just do the labels by hand.
+    plt.legend(handles=handles)
+    #title_string = r'$n^{(1)} = $' + n_pre_type# + ' and noise_master = ' + noise_master
+    # Do title by hand
+    title_string = 'Insert title here'
     plt.title(title_string)
     plt.show()
 
+#----- Should only need to edit below here ------
 
-# Idea inspired by https://scentellegher.github.io/programming/2017/07/15/pandas-groupby-multiple-columns-plot.html
 
+n_pre_type = 'constant'
+
+#noise_master = '(0.1, 0.0)' # To use with A
+noise_master = '(0.0, 0.3)' # To use with n
+
+ks = [10.0,20.0,30.0,40.0]
+
+#modifiers = ['(0.0, -1.0, 0.0, 0.0)','(0.0, -0.5, 0.0, 0.0)','(0.0, 0.0, 0.0, 0.0)'] # to use with A
+modifiers = ['(0.0, 0.0, 0.0, -1.0)','(0.0, 0.0, 0.0, -0.5)','(0.0, 0.0, 0.0, 0.0)'] # to use with n
+
+# ------ An example -------
+
+for ii in range(len(modifiers)):
+    plt_gmres('constant',noise_master,ks,modifiers[:(ii+1)])
 
                               
 
