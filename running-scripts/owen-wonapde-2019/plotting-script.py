96fa627c6dcd92fdd056c20cf834dcf460c6a6b7
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-01-17 14:43:29 +0000

    Updated plotting script.

--- running-scripts/owen-wonapde-2019/plotting-script.py
+++ running-scripts/owen-wonapde-2019/plotting-script.py
@@ -51,7 +51,7 @@ def plt_gmres(n_pre_type,noise_master,ks,modifiers,plot_num):
                 else:
                     plt.scatter(x=data.reset_index().loc[0,'k'],y=data.iloc[0,jj],c=cols[ii],marker=markers[ii],label='Modifier = '+modifier)
     labels = [r'$\mathrm{noise\,\,level}= 0.1/k$',r'$\mathrm{noise\,\,level} = 0.1/(k^{0.5})$',r'$\mathrm{noise\,\,level} = 0.1$']# noise = noise_master non-zero $k^whatever$ # for now I'll just do the labels by hand.
-    labels = labels[:(len(modifiers)+1)]
+    #labels = labels[:(len(modifiers)+1)]
     #plt.legend(handles,labels,loc=2)
     #title_string = r'$n^{(1)} = $' + n_pre_type# + ' and noise_master = ' + noise_master
     # Do title by hand
@@ -64,11 +64,11 @@ def plt_gmres(n_pre_type,noise_master,ks,modifiers,plot_num):
     #plot_num = len(modifiers)
     if noise_master == '(0.1, 0.0)':
         if plot_num == 1:
-            plt.yticks([7,8,9])
+            plt.yticks([6,7])
         elif plot_num == 2:
-            plt.yticks([10,20])
+            plt.yticks([5,10,15,20])
         elif plot_num == 3:
-            plt.yticks([40,80,120,160])
+            plt.yticks([50,100,150,200])
     elif noise_master == '(0.0, 0.1)':
         if plot_num == 1:
             plt.yticks([4,5,6])
@@ -91,8 +91,8 @@ def plt_gmres_A(A_pre_type,noise_master,ks,modifiers,plot_num):
                     handles.append(plt.scatter(x=data.reset_index().loc[0,'k'],y=data.iloc[0,jj],c=cols[ii],marker=markers[ii],label='Modifier = '+modifier))
                 else:
                     plt.scatter(x=data.reset_index().loc[0,'k'],y=data.iloc[0,jj],c=cols[ii],marker=markers[ii],label='Modifier = '+modifier)
-    labels = [r'$\mathrm{noise\,\,level}= 0.1/k$',r'$\mathrm{noise\,\,level} = 0.1/(k^{0.5})$',r'$\mathrm{noise\,\,level} = 0.1$']# noise = noise_master non-zero $k^whatever$ # for now I'll just do the labels by hand.
-    labels = labels[:(len(modifiers)+1)]
+    labels = [r'$\mathrm{noise\,\,level}= 0.05/k$',r'$\mathrm{noise\,\,level} = 0.05/(k^{0.5})$',r'$\mathrm{noise\,\,level} = 0.05$']# noise = noise_master non-zero $k^whatever$ # for now I'll just do the labels by hand.
+    #labels = labels[:(len(modifiers)+1)]
     #plt.legend(handles,labels,loc=2)
     #title_string = r'$n^{(1)} = $' + n_pre_type# + ' and noise_master = ' + noise_master
     # Do title by hand
@@ -103,20 +103,20 @@ def plt_gmres_A(A_pre_type,noise_master,ks,modifiers,plot_num):
     plt.xticks([20,40,60,80]) # told by http://stackoverflow.com/questions/12608788/ddg#12608937
     # Yes, this is a hack. No, I don't care.
     #plot_num = len(modifiers)
-    if noise_master == '(0.1, 0.0)':
+    if noise_master == '(0.05, 0.0)':
         if plot_num == 1:
-            plt.yticks([7,8,9])
+            plt.yticks([6,7])
         elif plot_num == 2:
-            plt.yticks([10,20])
+            plt.yticks([5,10,15,20])
         elif plot_num == 3:
-            plt.yticks([40,80,120,160])
+            plt.yticks([0,50,100,150,200,250])
     elif noise_master == '(0.0, 0.1)':
         if plot_num == 1:
             plt.yticks([4,5,6])
         elif plot_num == 2:
             plt.yticks([5,10,15])
         elif plot_num == 3:
-            plt.yticks([200,400,600,800,1000])
+            plt.yticks([0,200,400,600,800,1000])
     plt.show()
 
     
@@ -138,7 +138,7 @@ modifiers = ['(0.0, -1.0, 0.0, 0.0)','(0.0, -0.5, 0.0, 0.0)','(0.0, 0.0, 0.0, 0.
 
 for ii in range(len(modifiers)):
     print(modifiers[ii])
-#    plt_gmres(n_pre_type,noise_master,ks,modifiers[ii:(ii+1)],ii+1)
+#    plt_gmres(n_pre_type,noise_master,ks,modifiers[:(ii+1)],ii+1)
     plt_gmres_A(A_pre_type,noise_master,ks,modifiers[:(ii+1)],ii+1)
 
 
