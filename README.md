1dc0d017853b98b0f61a5e7ff3beeadb96e5800f
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-05-23 09:47:51 +0100

    Removed backend files from old qmc branch.

--- README.md
+++
@@ -1,8 +0,0 @@
-# helmholtz-nearby-preconditioning #
-
-Contains the experiments for the nearby preconditioning paper. Development ongoing.
-
-## Requirements ##
-
-1) Pandas
-2) Helmholtz Firedrake
--- helmholtz_nearby_preconditioning/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    import latticeseq_b2
-except ImportError:
-    from urllib.request import urlopen
-    with urlopen('https://bitbucket.org/dnuyens/qmc-generators/raw/cb0f2fb10fa9c9f2665e41419097781b611daa1e/python/latticeseq_b2.py') as qmc_module:
-        with open('latticeseq_b2.py',mode='x+b') as qmc_module_file:
-            qmc_module_file.write(qmc_module.read())
-
-    
