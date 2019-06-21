45fc5323a9bf7677d3d2ece46cf4eef3eafb2e8e
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-06-21 16:16:33 +0100

    Preliminary results to indicate that spatial gap of order 1/k might mean bounded GMRES iterations. Interesting!

--- small-changes-in-coeff/precon-discon.py
+++ small-changes-in-coeff/precon-discon.py
@@ -13,34 +13,39 @@ discon = np.array([[0.5,1.0],[0.0,1.0]])
 
 angle = 2.0*np.pi/3.0
 
-for eps in eps_list:
+#for eps in eps_list:
+
+eps_const = 0.5 # Looks like it gives you k-independent, at least up to k=40
+#eps_const = 1.0 # Nearly k-independent
+
+for k in k_list:
+
+    eps = eps_const/k
 
     shift = np.array([[eps,0.0],[0.0,0.0]])
-    
-    for k in k_list:
 
-        num_cells = h_to_num_cells(k**(-1.5),2)
+    num_cells = h_to_num_cells(k**(-1.5),2)
 
-        mesh = fd.UnitSquareMesh(num_cells,num_cells)
+    mesh = fd.UnitSquareMesh(num_cells,num_cells)
 
-        V = fd.FunctionSpace(mesh,"CG",1)
+    V = fd.FunctionSpace(mesh,"CG",1)
 
-        x = fd.SpatialCoordinate(mesh)
+    x = fd.SpatialCoordinate(mesh)
 
-        n = 0.5 + nd_indicator(x,1.0,discon+eps)
+    n = 0.5 + nd_indicator(x,1.0,discon+eps)
 
-        n_pre = 0.5 + nd_indicator(x,1.0,discon)
+    n_pre = 0.5 + nd_indicator(x,1.0,discon)
 
-        A = fd.as_matrix([[1.0,0.0],[0.0,1.0]])
+    A = fd.as_matrix([[1.0,0.0],[0.0,1.0]])
 
-        prob = HelmholtzProblem(k,V,A=A,n=n,A_pre=A,n_pre=n_pre)
+    prob = HelmholtzProblem(k,V,A=A,n=n,A_pre=A,n_pre=n_pre)
 
 
-        prob.f_g_plane_wave([np.cos(angle),np.sin(angle)])
+    prob.f_g_plane_wave([np.cos(angle),np.sin(angle)])
 
-        prob.solve()
+    prob.solve()
 
-        print('k',k,'eps',eps,'GMRES iterations',prob.GMRES_its,flush=True)
+    print('k',k,'eps',eps,'GMRES iterations',prob.GMRES_its,flush=True)
 
 
 
