792febef0bf236ac0e93015a72287fbcb529fd8a
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-07-22 11:18:15 +0100

    Removed most of 'old running scripts', moved bits that might be useful, added some files that weren't committed.

--- old-running-scripts/running-2.py
+++
@@ -1,8 +0,0 @@
-from helmholtz_nearby_preconditioning import experiments
-# For A changing
-#experiments.nearby_preconditioning_piecewise_experiment_set(A_pre_type='constant',n_pre_type='constant',dim=2,num_pieces=10,seed=1,num_repeats=20,k_list=[10.0,20.0,30.0],h_list=[(1.0,-1.5)],p_list=[1],noise_master_level_list=[(0.2,0.0)],noise_modifier_list=[(0.0,0.0,0.0,0.0),(0.0,-0.5,0.0,0.0),(0.0,-1.0,0.0,0.0)],save_location='./')
-
-# For n changing
-experiments.nearby_preconditioning_piecewise_experiment_set(A_pre_type='constant',n_pre_type='constant',dim=2,num_pieces=10,seed=1,num_repeats=20,k_list=[60.0],h_list=[(1.0,-1.5)],p_list=[1],noise_master_level_list=[(0.0,0.1)],noise_modifier_list=[(0.0,0.0,0.0,0.0),(0.0,0.0,0.0,-0.5),(0.0,0.0,0.0,-1.0)],save_location='./')
-
-
