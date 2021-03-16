0b83dc7489b0acdebbb19edbae625c96215d9319
Owen Pembery <o.r.pembery@bath.ac.uk>
2021-03-16 12:07:24 +0000

    Modified running scripts in order to do extra run for k=100 that was missed before

--- nbpc-scaling-linfinity/running-nbpc-scaling-linfinity.py
+++ nbpc-scaling-linfinity/running-nbpc-scaling-linfinity.py
@@ -33,11 +33,13 @@ save_location = './output/'
 noise_master_level = 0.5
 
 # For A changing
-experiments.nearby_preconditioning_piecewise_experiment_set(A_pre_type=A_pre_type,n_pre_type=n_pre_type,dim=dim,num_pieces=num_pieces,seed=seed,num_repeats=num_repeats,k_list=k_list,h_list=h_list,p_list=p_list,noise_master_level_list=[(noise_master_level,0.0)],noise_modifier_list=[(0.0,0.0,0.0,0.0),(0.0,-0.1,0.0,0.0),(0.0,-0.2,0.0,0.0),(0.0,-0.3,0.0,0.0),(0.0,-0.4,0.0,0.0),(0.0,-0.5,0.0,0.0),(0.0,-0.6,0.0,0.0),(0.0,-0.7,0.0,0.0),(0.0,-0.8,0.0,0.0),(0.0,-0.9,0.0,0.0),(0.0,-1.0,0.0,0.0)],save_location=save_location)
+#experiments.nearby_preconditioning_piecewise_experiment_set(A_pre_type=A_pre_type,n_pre_type=n_pre_type,dim=dim,num_pieces=num_pieces,seed=seed,num_repeats=num_repeats,k_list=k_list,h_list=h_list,p_list=p_list,noise_master_level_list=[(noise_master_level,0.0)],noise_modifier_list=[(0.0,0.0,0.0,0.0),(0.0,-0.1,0.0,0.0),(0.0,-0.2,0.0,0.0),(0.0,-0.3,0.0,0.0),(0.0,-0.4,0.0,0.0),(0.0,-0.5,0.0,0.0),(0.0,-0.6,0.0,0.0),(0.0,-0.7,0.0,0.0),(0.0,-0.8,0.0,0.0),(0.0,-0.9,0.0,0.0),(0.0,-1.0,0.0,0.0)],save_location=save_location)
 
 print("A complete")
 
 # For n changing
-experiments.nearby_preconditioning_piecewise_experiment_set(A_pre_type=A_pre_type,n_pre_type=n_pre_type,dim=dim,num_pieces=num_pieces,seed=seed,num_repeats=num_repeats,k_list=k_list,h_list=h_list,p_list=p_list,noise_master_level_list=[(0.0,noise_master_level)],noise_modifier_list=[(0.0,0.0,0.0,0.0),(0.0,0.0,0.0,-0.1),(0.0,0.0,0.0,-0.2),(0.0,0.0,0.0,-0.3),(0.0,0.0,0.0,-0.4),(0.0,0.0,0.0,-0.5),(0.0,0.0,0.0,-0.6),(0.0,0.0,0.0,-0.7),(0.0,0.0,0.0,-0.8),(0.0,0.0,0.0,-0.9),(0.0,0.0,0.0,-1.0)],save_location=save_location)
+#experiments.nearby_preconditioning_piecewise_experiment_set(A_pre_type=A_pre_type,n_pre_type=n_pre_type,dim=dim,num_pieces=num_pieces,seed=seed,num_repeats=num_repeats,k_list=k_list,h_list=h_list,p_list=p_list,noise_master_level_list=[(0.0,noise_master_level)],noise_modifier_list=[(0.0,0.0,0.0,0.0),(0.0,0.0,0.0,-0.1),(0.0,0.0,0.0,-0.2),(0.0,0.0,0.0,-0.3),(0.0,0.0,0.0,-0.4),(0.0,0.0,0.0,-0.5),(0.0,0.0,0.0,-0.6),(0.0,0.0,0.0,-0.7),(0.0,0.0,0.0,-0.8),(0.0,0.0,0.0,-0.9),(0.0,0.0,0.0,-1.0)],save_location=save_location)
+# The following is to do the one extra run that was missed off
+experiments.nearby_preconditioning_piecewise_experiment_set(A_pre_type=A_pre_type,n_pre_type=n_pre_type,dim=dim,num_pieces=num_pieces,seed=seed,num_repeats=num_repeats,k_list=k_list,h_list=h_list,p_list=p_list,noise_master_level_list=[(0.0,noise_master_level)],noise_modifier_list=[(0.0,0.0,0.0,-1.0)],save_location=save_location)
 
 print("n complete")
