10745e57b8807d8c56fe001454b1d8f4e63c4794
Owen Pembery <opembery@gmail.com>
2021-03-17 12:29:26 +0000

    Initial attempt

---
+++ nbpc-scaling-linfinity/running-deterministic-scaling-linfinty.py
@@ -0,0 +1,101 @@
+import sys
+import helmholtz_firedrake.problems as hh
+import helmholtz_firedrake.utils as hh_utils
+import firedrake as fd
+import numpy as np
+
+# This is a slightly rough-and-ready experiment to assess whether just taking the worst case on an alternating checkerboard gives substantially different numbers of GMRES iterations to those already in the paper.
+# It borrows from other code, but has had to be reworked for the deterministic scenario
+# Three input arguments: k, alpha, bool (1 if want to do A computation, 0 if n),  fourth is 1 if on balena, 0 otherwise
+
+on_balena = bool(int(sys.argv[4]))
+
+if on_balena:
+    from firedrake_complex_hacks.balena_hacks import fix_mesh_generation_time
+    fix_mesh_generation_time()
+
+k = float(sys.argv[1])
+
+alpha = float(sys.argv[2])
+
+A_vs_n = bool(sys.argv[3])
+
+dim = 2
+
+h = k**(-1.5)
+
+mesh_points = hh_utils.h_to_num_cells(h,dim)
+mesh = fd.UnitSquareMesh(mesh_points,mesh_points)
+V = fd.FunctionSpace(mesh, "CG", 1)
+
+f = 0.0
+d = fd.as_vector([1.0/fd.sqrt(2.0),1.0/fd.sqrt(2.0)])
+x = fd.SpatialCoordinate(mesh)
+nu = fd.FacetNormal(mesh)
+g = 1j*k*fd.exp(1j*k*fd.dot(x,d))*(fd.dot(d,nu)-1)
+
+if A_vs_n:
+    constant_to_multiply = fd.as_matrix([[1.0,0.0],[0.0,1.0]])
+    varying_coeff = fd.as_matrix([[0.0,0.0],[0.0,0.0]])
+else:
+    constant_to_multiply = 1.0
+    varying_coeff = 0.0
+
+# This is, strictly speaking, the number of subdomains in each direction
+subdomains = 10
+
+for ii in range(subdomains):
+    for jj in range(subdomains):
+        if np.mod(ii+jj,2) == 1:
+            value = 1 + alpha
+        else:
+            value = 1 - alpha
+            fl_ii = float(ii)
+            fl_jj = float(jj)
+        varying_coeff += hh_utils.nd_indicator(x,value * constant_to_multiply,np.array([[fl_ii,fl_ii+1.0],[fl_jj,fl_jj+1.0]])/float(subdomains))
+
+if A_vs_n:        
+    A = varying_coeff
+    n = 1.0
+else:
+    A = fd.as_matrix([[1.0,0.0],[0.0,1.0]])
+    n = varying_coeff
+
+A_pre = fd.as_matrix([[1.0,0.0],[0.0,1.0]])
+
+n_pre = 1.0
+
+prob = hh.HelmholtzProblem(k=k, V=V, A=A, n=n, A_pre=A_pre, n_pre=n_pre)
+
+
+
+prob.solve()
+
+all_GMRES_its = []
+
+if fd.COMM_WORLD.rank == 0:
+        
+    all_GMRES_its.append(prob.GMRES_its)
+
+all_GMRES_its  = np.array(all_GMRES_its)
+
+# Write this to file
+
+if fd.COMM_WORLD.rank == 0:
+    hh_utils.write_GMRES_its(
+        GMRES_its,save_location,
+        {'k' : k,
+         'h_tuple' : h_tuple,
+         'p' : p,
+         'num_pieces' : num_pieces,
+         'A_pre_type' : A_pre_type,
+         'n_pre_type' : n_pre_type,
+         'noise_master' : noise_master,
+         'modifier' : modifier,
+         'num_repeats' : num_repeats
+         }
+    )
+
+    
+
+
