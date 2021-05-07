181e9b46bba4eac650a7dedebeec48deae7c40d2
Owen Pembery <o.r.pembery@bath.ac.uk>
2021-05-07 19:41:21 +0100

    Doing k=200 runs

--- nbpc-scaling-linfinity/running-deterministic-scaling-linfinity-balena.sh
+++ nbpc-scaling-linfinity/running-deterministic-scaling-linfinity-balena.sh
@@ -2,7 +2,7 @@
 
 source /beegfs/scratch/user/s/orp20/own-utilities/helmholtz_firedrake_venv.sh
 
-for k in  20.0 40.0 60.0 80.0 100.0 120.0 140.0 160.0 180.0
+for k in  200.0 #20.0 40.0 60.0 80.0 100.0 120.0 140.0 160.0 180.0
 do
     for beta in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
     do
